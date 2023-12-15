0d0ad92b638754be8951bad21fddbab5f55cfb94
nezhdanchik <kirdanchik@yandex.ru>
2023-12-15 22:21:48 +0300

    bug fix (неожиданное поведение в конце игры)

--- Tic-tac-toe/main.py
+++ Tic-tac-toe/main.py
@@ -3,15 +3,22 @@ from board import Board, Emotion
 
 class Game:
     def __init__(self):
-        size = int(input("Сторона поля ? "))
-        if not 2 <= size <= 11:
-            print(Emotion.add_exclamation("Должно выполняться условие 2 <= size <= 11"))
-            self.__init__()
+        try:
+            size = int(input("Сторона поля ? "))
+            if not 2 <= size <= 11:
+                print(Emotion.add_exclamation("Должно выполняться условие 2 <= size <= 11"))
+                self.__init__()
+                return
 
-        count = int(input("Количество фигур в ряд ? "))
-        if count > size:
-            print(Emotion.add_exclamation("Должно выполняться условие count_to_win <= size"))
+            count = int(input("Количество фигур в ряд ? "))
+            if count > size:
+                print(Emotion.add_exclamation("Должно выполняться условие count_to_win <= size"))
+                self.__init__()
+                return
+        except ValueError:
+            print(Emotion.add_exclamation('Неверный формат ввода параметра'))
             self.__init__()
+            return
 
         self.board = Board(size, count)
 
@@ -24,8 +31,12 @@ class Game:
     def game_loop(self):
         self.board.print_field()
         while True:
-            cordinates = list(map(int, input(f"Ход {'крестиков' if self.stage % 2 == 0 else 'ноликов'}."
-                                             f" Введите координаты по оси x и y через пробел: ").split()))
+            try:
+                cordinates = list(map(int, input(f"Ход {'крестиков' if self.stage % 2 == 0 else 'ноликов'}."
+                                                 f" Введите координаты по оси x и y через пробел: ").split()))
+            except ValueError:
+                print(Emotion.add_exclamation('Координаты введены неверно'))
+                continue
 
             success = self.board.set_figure(self.stage, cordinates)  # Был ли сделан ход
             if success:
@@ -37,12 +48,16 @@ class Game:
                     print(win)
                     if input('Хотите сыгать ещё? Y/N ') == 'Y':
                         self.__init__()
-                    break
+                        return
+                    else:
+                        print('Спасибо за игру')
+                        break
 
                 if self.board.check_draw(self.stage):
                     print(Emotion.add_indifference('Ничья'))
                     if input('Хотите сыгать ещё? Y/N ') == 'Y':
                         self.__init__()
+                        return
                     break
 
 
